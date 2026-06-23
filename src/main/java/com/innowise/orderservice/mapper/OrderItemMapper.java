68f68da1a95f8878ef339b32427b634b60599632
victoria <vasharchuk07@gmail.com>
2026-06-23 14:54:33 +0300

    fix exception with source

--- src/main/java/com/innowise/orderservice/mapper/OrderItemMapper.java
+++ src/main/java/com/innowise/orderservice/mapper/OrderItemMapper.java
@@ -2,10 +2,12 @@ package com.innowise.orderservice.mapper;
 
 import com.innowise.orderservice.dto.OrderItemResponse;
 import com.innowise.orderservice.entity.OrderItemEntity;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface OrderItemMapper {
@@ -14,6 +16,17 @@ public interface OrderItemMapper {
     @Mapping(target = "itemName", source = "item.name")
     @Mapping(target = "itemPrice", source = "item.price")
     @Mapping(target = "quantity", source = "quantity")
-    @Mapping(target = "subtotal", expression = "java(calculateSubtotal(entity))")
+    @Mapping(target = "subtotal", ignore = true)
     OrderItemResponse toDto(OrderItemEntity item);
+
+
+    List<OrderItemResponse> toDtoList(List<OrderItemEntity> entities);
+
+    @AfterMapping
+    default BigDecimal calculateSubtotal(OrderItemEntity entity) {
+        if (entity == null || entity.getItem() == null || entity.getQuantity() == null) {
+            return BigDecimal.ZERO;
+        }
+        return entity.getItem().getPrice().multiply(BigDecimal.valueOf(entity.getQuantity()));
+    }
 }
